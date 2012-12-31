ac5ccd51ed63f0a27e72e62a69db917abdfb0b57
Pablo Trabajos <pablo.trabajos.tamayo@gmail.com>
2012-12-31 05:42:45 +0100

    added User to admin.py

--- client_list/admin.py
+++ client_list/admin.py
@@ -1,5 +1,5 @@
 ﻿from django.contrib import admin
-from farproof.client_list.models import Client, Job, Item, Page, Revision, Comment, Curve, ProviderContent, ClientContent
+from farproof.client_list.models import Client, User, Job, Item, Page, Revision, Comment, Curve, ProviderContent, ClientContent
 
 class ItemAdmin(admin.ModelAdmin):
     fieldsets = [
@@ -9,6 +9,7 @@ class ItemAdmin(admin.ModelAdmin):
     ]
 
 admin.site.register(Client)
+admin.site.register(User)
 admin.site.register(Job)
 admin.site.register(Item, ItemAdmin)
 admin.site.register(Page)
