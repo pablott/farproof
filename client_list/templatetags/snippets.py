21f8b791d7f552fa69a0b8a268de21603545ebc1
Pablo Trabajos <pablo.trabajos.tamayo@gmail.com>
2013-01-10 20:00:17 +0100

    turned Client into an inclusion_tag

--- client_list/templatetags/snippets.py
+++ client_list/templatetags/snippets.py
@@ -3,6 +3,12 @@ from django import template
 register = template.Library()
 
 
+@register.inclusion_tag('snippets/client.html')
+def show_client(client):
+	return {
+		'client': client
+	}
+
 @register.inclusion_tag('snippets/job.html')
 def show_job(job, client):
 	return {
