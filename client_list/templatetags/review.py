f4398a71afc4c61470ef18bd54675fa52d8fa096
Pablo Trabajos <pablo.trabajos.tamayo@gmail.com>
2013-01-18 01:19:53 +0100

    added review.py custom template tag

---
+++ client_list/templatetags/review.py
@@ -0,0 +1,34 @@
+from django import template
+
+register = template.Library()
+
+
+@register.inclusion_tag('snippets/affects_too.html')
+def affects_too(comment, page, item, job, client):
+	revisions = comment.revision.all() # Get all revisions with that associated comment
+	last_rev = page.last_rev()
+	revisions = revisions.exclude(pk=last_rev.pk) # Exclude revision associated to 'page'
+	#revisions = revisions.exclude(pk=last_rev.pk)
+	
+	#affected_pages = page.revisions_set
+	
+	#for revisions
+	#	if revision == revision.page.last_rev
+	#		rev_list = rev_list + revision
+
+	#TODO get only last revisions
+	
+	#for revision in revisions:
+	#	affected_pages = revision.page
+		
+	return {
+		'revisions': revisions,
+		#'rev_list': rev_list,
+		'item': item,
+		'job': job, 
+		'client': client,
+	#	'affected_pages': affected_pages,
+	#	'page': page
+	}
+
+	
