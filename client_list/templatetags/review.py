9bdc59d2c0892ce7e883fb07dc53c57b48b5779e
Pablo Trabajos <pablo.trabajos.tamayo@gmail.com>
2013-02-18 19:50:55 +0100

    cleanup review.py templatetag

--- client_list/templatetags/review.py
+++ client_list/templatetags/review.py
@@ -6,29 +6,14 @@ register = template.Library()
 @register.inclusion_tag('widgets/affects_too.html')
 def affects_too(comment, page, item, job, client):
 	revisions = comment.revision.all() # Get all revisions with that associated comment
-	last_rev = page.last_rev()
-	revisions = revisions.exclude(pk=last_rev.pk) # Exclude revision associated to 'page'
-	#revisions = revisions.exclude(pk=last_rev.pk)
-	
-	#affected_pages = page.revisions_set
-	
-	#for revisions
-	#	if revision == revision.page.last_rev
-	#		rev_list = rev_list + revision
-
-	#TODO get only last revisions
-	
-	#for revision in revisions:
-	#	affected_pages = revision.page
+	last_rev = page.last_rev() # Get last revision for current page
+	revisions = revisions.exclude(pk=last_rev.pk) # Finally, exclude last_rev itself from list
 		
 	return {
 		'revisions': revisions,
-		#'rev_list': rev_list,
 		'item': item,
 		'job': job, 
 		'client': client,
-	#	'affected_pages': affected_pages,
-	#	'page': page
 	}
 
-	
+
