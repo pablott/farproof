d9c08ab8234e74c0f6bf5268cae274c7b9ed6335
Pablo Trabajos <pablo.trabajos.tamayo@gmail.com>
2013-03-03 10:24:24 +0100

    tidied process.py

--- process/process.py
+++ process/process.py
@@ -3,38 +3,70 @@ import subprocess
 
 # See: http://ghostscript.com/doc/current/Use.htm
 # and: http://ghostscript.com/doc/current/Devices.htm#TIFF
+# and: http://ghostscript.com/doc/current/GS9_Color_Management.pdf
 
-gs = 'C:/Users/Pablo/Ghostscript/bin/gswin32c.exe'
+# CAUTION: is very important that variables are written like this:
+#'-var1 -var2', (with the '' included)
+gs = r'D:\tmp\gs\bin\gswin64c.exe',
 #PATH = "D:/tmp/"
-COMMON = '-dSAFER', '-dNOPAUSE', '-dBATCH', #'-dQUIET',
+COMMON = '-dNOPAUSE -dBATCH -dQUIET', # REQUIRES the trailing comma for some odd reason
 
 # Render Options:
-GRAPHICS = '-dGraphicsAlphaBits=2', 
+GRAPHICS = '-dGraphicsAlphaBits=2'
 JPEGQ = '-dJPEGQ=95'
-TEXT = '-dTextAlphaBits=4', #'-dAlignToPixels=0', 
-COLOR = '-dUseCIEColor', '-dDOINTERPOLATE',  '-dCOLORSCREEN', 
+TEXT = '-dTextAlphaBits=4 -dAlignToPixels=0'
+COLOR = '-dUseCIEColor -dDOINTERPOLATE' #-dCOLORSCREEN'
 
 # Color Management: 
-#INPUT_PROFILE = sRGB|AdobeRGB
-#OUTPUT_PROFILE = '-sOutputICCProfile=filename'
-#RENDER_INTENT = '-dRenderIntent=0/1/2/3' #relative|absolute|perceptual
-#BPC = '-dRenderIntent=0/1/2/3'
-#K_PRESERVE = 0/1/2
+ICC_FOLDER = '-sICCProfilesDir=' "D:/tmp/profiles/"
+RGB_PROFILE = '-sOutputICCProfile=sRGB.icm'
+CMYK_PROFILE = '-sOutputICCProfile=CoatedFOGRA27.icc' #-sProofProfile
+RENDER_INTENT = '-dRenderIntent=1' #0:Perceptual, 1:Colorimetric, 2:Saturation, 3:Absolute Colorimetric
+OVERPRINT = '-dSimulateOverprint=true' #Only for CMYK outputs
+BPC = '-dBlackPtComp=1' #0:Don't, #1:Do
+PRESERVE_K = '-dKPreserve=0' #0:No preservation, 1:PRESERVE K ONLY (littleCMS), 2:PRESERVE K PLANE (littleCMS)
 
 def handle_uploaded_file(dpi): 
+	#TODO: RGB devices don't support overprint, conversion from CMYK tiff is neccesary
 	subprocess.Popen([
-		gs, 
+		gs,
+		'-sDEVICE=jpeg', '-r' + str(dpi),
 		COMMON,
-		'-sDEVICE=jpeg', '-r' + str(dpi) + 'x' + str(dpi), 
-		TEXT, GRAPHICS, COLOR, JPEGQ,
-		'-sOUTPUTFILE=' "D:/tmp/result.jpg", "D:/tmp/source.pdf"
-	], shell=True)	
+		GRAPHICS,
+		TEXT,
+		JPEGQ,
+		#RENDER_INTENT,
+		#ICC_FOLDER, RGB_PROFILE, #BPC,
+		#COLOR, 
+		'-sOUTPUTFILE=' "D:/tmp/result.jpg", "D:/tmp/source.pdf",
+	])
+	# TODO: make CMYK_PROFILE and OVERPRINT work together
+	subprocess.Popen([
+		gs,
+		'-sDEVICE=tiffsep', '-r' + str(dpi),
+		COMMON,
+		COLOR, 
+		GRAPHICS,
+		TEXT,
+		RENDER_INTENT,
+		#ICC_FOLDER, CMYK_PROFILE, 
+		#BPC,
+		#PRESERVE_K,
+		OVERPRINT,
+		'-sOUTPUTFILE=' "D:/tmp/tiffsep/result.tiff", "D:/tmp/source.pdf", #'-sstdout=' "D:/tmp/file.txt",
+	]) 
 	
+	#subprocess.Popen([
+	#	gs,
+	#	'-o', '-sDEVICE=inkcov', "D:/tmp/source.pdf",
+	#], shell=True) 
+
+#TODO: just delete this function
+def handle_uploaded_file2(dpi): 
 	subprocess.Popen([
 		gs, 
-		COMMON, 
-		'-sDEVICE=tiffsep', '-r' + str(dpi) + 'x' + str(dpi), 
-		TEXT, GRAPHICS, COLOR, 
-		'-sOUTPUTFILE=' "D:/tmp/tiffsep/result.tiff", "D:/tmp/source.pdf", '-sstdout=' "D:/tmp/file.txt"
-	], shell=True)	
+		'-dNOPAUSE', '-dBATCH', '-dQUIET', 
+		'-sDEVICE=jpeg', '-sOUTPUTFILE=' "D:/tmp/resultqq.jpg", "D:/tmp/source.pdf"
+	], shell=True)
+
 	
