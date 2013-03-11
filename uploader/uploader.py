b68790370330abfc4d9bddb74f125cca64619aca
Pablo Trabajos <pablo.trabajos.tamayo@gmail.com>
2013-03-11 06:39:06 +0100

    now uploader gets every file

--- uploader/uploader.py
+++ uploader/uploader.py
@@ -1,12 +1,13 @@
-import subprocess
 from django.shortcuts import render_to_response # Add get_object_or_404
-from farproof.client_list.models import Client, Job, Item, Page
-from farproof.client_list.models import UploadFileForm
-
+from farproof.client_list.models import Client, Job, Item, Page, Revision
 from farproof.process.process import *
 
+import os, subprocess
 
-PATH = "D:/tmp/"
+
+#TODO: JSON and MD5 client-side checksum verified by server-side checksum
+
+PDF_PATH = "D:/tmp/pdf/"
 
 
 def file_upload(request, client_pk, job_pk, item_pk, page_num):
@@ -16,25 +17,96 @@ def file_upload(request, client_pk, job_pk, item_pk, page_num):
 	page = Page.objects.get(number=page_num, item=item)
 
 	if request.method == 'POST':
-		form = UploadFileForm(request.POST, request.FILES)
-		if form.is_valid():
-			message = "file sent"
-			write_file(request.FILES['uploads'])
+		#form = request.FILES # bind to a form
+		#if form.is_valid():
+		upload_list = request.FILES.getlist('uploads') # this is a MultiValueDict 
+		write_file(upload_list, client, job, item, page)
+		message = "Uploaded files: "
+		total_size = 0
+		for file in upload_list:
+			message = message +" - "+ file.name +" - "+ file.content_type +" - "+ str(round(file.size/1048576.0, 2))+"MB"
+			total_size = total_size + file.size
+		message = message +" / "+ str(total_size/1048576.0) + "MB"
 	else:
+		upload_list = "empty"
 		message = "upload something"
-		form = UploadFileForm()
 	return render_to_response('file_upload.html', {
 		'client': client,
-		'job': job, 
+		'job': job,
 		'item': item,
 		'page': page,
-		'form': form,
+		'upload_list': upload_list,
 		'message': message,
 	})
 
-def write_file(f):
-	with open(PATH + 'upload.pdf', 'wb+') as destination:
-		for chunk in f.chunks():
-			destination.write(chunk)
-	process(300)
-			
+	
+def write_file(upload_list, client, job, item, page):
+	dirname = PDF_PATH + str(client.pk) +"/"+ str(job.pk) +"/"+ str(item.pk) +"/"+ str(page.pk) +"/"+ str(page.last_rev().rev_number+1) +"/"
+	os.makedirs(dirname) # TODO: don't stop on OSError and jump to writing chunks
+	#os.mkdir(os.path.join(PDF_PATH, dirname)) #TODO: remove
+
+	for file in upload_list:
+		filename = file.name
+		with open(dirname + filename, 'wb+') as destination:
+			for chunk in file.chunks():
+				destination.write(chunk)
+		process(150, dirname, filename)
+
+
+# add PDF to last rev of a page:
+# create a temp file with pdf
+# read first figure from filename (this is the starting absolute position)
+# get number of pages from pdf (use pyPDF)
+# construct array: number of pages = [first_page,pdf_num_pages]
+# run gs and output all pages to a temp tiff (optional: run separations)
+# A: run cmyk tiff to rgb jpeg
+# B: run cmyk sep to rgb png
+# folder structure:
+# /client/job/item/page/rev/: in this folder only files related to page previews
+# /client/job/item/page/rev/render/render.jpg: page previews in RGB jpg
+# /client/job/item/page/rev/seps/C|M|Y|K|S(N).png: page separations in rgb png
+# /client/job/item/pdf/date?/original filename.pdf: storage for uploaded pdf
+
+
+# with number of pages, create a new revision assigned to each page starting from first_page
+# each Revision has a FileField attached to it
+# - FileField for render.jpg
+# - FileField for seps png
+# - FileField for uploaded PDF
+# FileField is filled with path /client/job/item/page/rev/
+# so assignation works like: N-result.jpg <-> N-original.pdf <-> N-seps.tiff <-> N-Page item 
+# where N is the abs. page number
+# there is another FileField for Revision for the uploaded pdf
+# that points to /client/job/item/pdf/date?/original filename.pdf
+
+
+# then user can add Comment to Revision using comment system
+# create directory based on client.pk/job.pk/item.pk/page_num/rev
+
+# FUNCTIONS:
+# upload (upload_list, dirname, filename)
+# upload file to temp folder 
+# returns (uploaded_file)
+
+# process (uploaded_file)
+# create cmyk tiff->seps & jpg render in a temp folder
+# returns (processed_files, seps)
+
+# rename (processed_files, seps)
+# copies file to real folder in /client/job/item/page/rev/render|seps
+# move uploaded pdf somewhere safe
+# returns (Revision with processed_file & uploaded_file attached)
+
+# process_spots (seps)
+# interface for approving which spot colors get loaded 
+# returns save2item_conf (list of spot colors)
+
+# comment (Revision)
+# gets last created Revision for that page and adds Comment before publishing
+# returns (Comment associated to Revision)
+
+	
+#file.name           # Gives name
+#file.content_type   # Gives Content type text/html etc
+#file.size           # Gives file's size in byte
+#file.read() 	
