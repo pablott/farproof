bfa979a64d41d42258438b2ef7d7a902d388d632
Pablo Trabajos <pablo.trabajos.tamayo@gmail.com>
2013-10-21 21:05:58 +0200

    added django-debug-toolbar

--- settings.py
+++ settings.py
@@ -108,6 +108,7 @@ MIDDLEWARE_CLASSES = (
 	#'django.middleware.csrf.CsrfViewMiddleware',
 	'django.contrib.auth.middleware.AuthenticationMiddleware',
 	'django.contrib.messages.middleware.MessageMiddleware',
+	'debug_toolbar.middleware.DebugToolbarMiddleware',
 )
 
 # https://docs.djangoproject.com/en/1.3/howto/static-files/#with-a-context-processor
@@ -191,6 +192,21 @@ INSTALLED_APPS = (
 	'django_evolution',
 	'dajaxice',
 	'dajax',
+	'debug_toolbar',
 )
+
+DEBUG_TOOLBAR_PANELS = (
+	'debug_toolbar.panels.version.VersionDebugPanel',
+	'debug_toolbar.panels.timer.TimerDebugPanel',
+	'debug_toolbar.panels.settings_vars.SettingsVarsDebugPanel',
+	'debug_toolbar.panels.headers.HeaderDebugPanel',
+	'debug_toolbar.panels.request_vars.RequestVarsDebugPanel',
+	'debug_toolbar.panels.template.TemplateDebugPanel',
+	'debug_toolbar.panels.sql.SQLDebugPanel',
+	'debug_toolbar.panels.signals.SignalDebugPanel',
+	'debug_toolbar.panels.logger.LoggingPanel',
+)
+
+INTERNAL_IPS = ('127.0.0.1',)
 
 DAJAXICE_DEBUG = True
