83e0bb3c6bd483c1c5be89951149e280b4c21e24
Pablo Trabajos <pablo.trabajos.tamayo@gmail.com>
2013-10-23 21:34:32 +0200

    commented user database (not in use right now)

--- settings.py
+++ settings.py
@@ -22,20 +22,20 @@ MANAGERS = ADMINS
 DATABASES = {
 	'default': {
 		'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-		'NAME': 'jobs.sqlite',                      # Or path to database file if using sqlite3.
-		'USER': '',                      # Not used with sqlite3.
-		'PASSWORD': '',                  # Not used with sqlite3.
-		'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
-		'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
+		'NAME': 'jobs.sqlite',					# Or path to database file if using sqlite3.
+		'USER': '',								# Not used with sqlite3.
+		'PASSWORD': '',							# Not used with sqlite3.
+		'HOST': '',								# Set to empty string for localhost. Not used with sqlite3.
+		'PORT': '',								# Set to empty string for default. Not used with sqlite3.
 	},
-	'users': {
-		'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-		'NAME': 'users.sqlite',                      # Or path to database file if using sqlite3.
-		'USER': '',                      # Not used with sqlite3.
-		'PASSWORD': '',                  # Not used with sqlite3.
-		'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
-		'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
-	}
+	# 'users': {
+		# 'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
+		# 'NAME': 'users.sqlite',          # Or path to database file if using sqlite3.
+		# 'USER': '',                      # Not used with sqlite3.
+		# 'PASSWORD': '',                  # Not used with sqlite3.
+		# 'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
+		# 'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
+	# }
 }
 
 # Local time zone for this installation. Choices can be found here:
