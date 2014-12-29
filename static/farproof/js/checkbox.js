843d7898e8984d22c954006238d139efc8d7e4fe
Pablo Trabajos <pablo.trabajos.tamayo@gmail.com>
2014-12-29 21:56:25 +0100

    merged static content from 'farproof-html' repo

---
+++ static/farproof/js/checkbox.js
@@ -0,0 +1,31 @@
+// http://briancray.com/posts/check-all-jquery-javascript
+
+$(function () {
+    $('.checkall').click(function () {
+<<<<<<< HEAD
+        $('.list_container:eq(0)').find(':checkbox').prop('checked', this.checked);
+        alert(page_set);
+    });
+
+    $('.list_container :checkbox').click(function () {
+        var checked = 0;
+        var total_pages = 0;
+        for $('.list_container') in (.find(':checked')) {
+            checked = checked + 1;
+            total_pages = total_pages + 1;
+        }
+        ;
+
+        if checked = total_pages {
+            alert('sdasd');
+            $('.checkall').class('debug');
+
+        }
+    });
+
+=======
+        $('.list_container:eq(0)').find(':checkbox').attr('checked', this.checked);
+    });
+>>>>>>> 17b09ee... INITIAL COMMIT!!
+});
+    
---
+++ static/farproof/js/nav.js
@@ -0,0 +1,11 @@
+<<<<<<< HEAD
+function zoomIn() {
+=======
+function zoomIn () {
+>>>>>>> 17b09ee... INITIAL COMMIT!!
+    $("#zoomIn").click(function () {
+        $("#page_container").addClass("zoomed");
+        $(".debug").html("1");
+    });
+
+}
